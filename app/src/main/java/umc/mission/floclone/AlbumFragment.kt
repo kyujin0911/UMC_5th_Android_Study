68c708ee34288269ce10b6e7e55701decf23a0e7
KKJ <kyujin0911@tukorea.ac.kr>
2023-09-29 00:51:48 +0900

    2주차 미션-SongActivity xml 구현 완료

---
+++ app/src/main/java/umc/mission/floclone/AlbumFragment.kt
@@ -0,0 +1,7 @@
+package umc.mission.floclone
+
+import androidx.fragment.app.Fragment
+
+class AlbumFragment: Fragment() {
+
+}
---
+++ app/src/main/java/umc/mission/floclone/SongActivity.kt
@@ -0,0 +1,11 @@
+package umc.mission.floclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SongActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_song)
+    }
+}
